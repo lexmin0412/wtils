43ed2a86252bdba8e483205f6aa9ed4314b7ecaa
cathe-zhang <zhangle_media@hotmail.com>
2019-08-14 20:57:39 +0800

    chore: 打包时压缩代码

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
@@ -10,12 +11,13 @@ export default {
   },
   plugins:[
     resolve(),  // 解析node模块(rollup默认不支持)
+    commonjs({   //  commonjs模块支持
+      include: 'node_modules/**', // 包括
+      exclude: [],  // 排除
+    }),    
     babel({  // 运行babel配置
       exclude: '**/node_modules/**'   // 不打包node_modules中的文件
     }),
-    commonjs({
-      include: 'node_modules/**', // 包括
-      exclude: [],  // 排除
-    }),
+    uglify(),  // 压缩
   ],
 }
