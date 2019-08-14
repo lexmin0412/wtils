99d2beeb2d95050d34f792319d220ad427c648fc
cathe-zhang <zhangle_media@hotmail.com>
2019-08-14 20:40:06 +0800

    chore: 新增node模块支持，引入babel编译ES6语法

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,7 +1,21 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins:[
+    resolve(),  // 解析node模块(rollup默认不支持)
+    babel({  // 运行babel配置
+      exclude: '**/node_modules/**'   // 不打包node_modules中的文件
+    }),
+    commonjs({
+      include: 'node_modules/**', // 包括
+      exclude: [],  // 排除
+    }),
+  ],
 }
