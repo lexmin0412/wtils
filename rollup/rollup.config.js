cfb1db2f5dcc31e61185e41adba79d4308cfb289
cathe-zhang <zhangle_media@hotmail.com>
2019-08-15 12:39:50 +0800

    chore: 引入rollup-plugin-filesize插件

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
 
 export default {
   input: 'src/index.js',
@@ -19,5 +20,6 @@ export default {
       exclude: '**/node_modules/**'   // 不打包node_modules中的文件
     }),
     uglify(),  // 压缩
+    filesize(),  // 打包完毕后显示文件大小
   ],
 }
